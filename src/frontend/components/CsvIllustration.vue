1633de9ea6d50c433551d14e6bd026b81ec95b6e
Patrick Raedler <patrick.raedler@schroedinger-hat.org>
2025-06-28 23:12:41 +0200

    feat(components): create components to hold svgs illustrations

---
+++ src/frontend/components/CsvIllustration.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Csv from '~/assets/svg/csv.vue'
+</script>
+
+<template>
+  <div class="relative">
+    <!-- Purple circle background -->
+    <div class="absolute right-0 top-0 w-4/5 h-4/5 bg-[#8B5CF6] rounded-full opacity-90" />
+
+    <!-- CSV Illustration -->
+    <div class="relative">
+      <Csv />
+    </div>
+  </div>
+</template>
