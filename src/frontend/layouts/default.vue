bd13502a967c0c7dba33cfa19881fa07b353e875
Marco <marco.giuliotti@gmail.com>
2025-07-01 18:57:25 +0200

    style(default.vue): restore main margin bottom after remove margin top from footer

--- src/frontend/layouts/default.vue
+++ src/frontend/layouts/default.vue
@@ -233,7 +233,7 @@ const navLinks = [
     </nav>
 
     <!-- Main Content -->
-    <main class="pt-16 flex-grow flex flex-col">
+    <main class="pt-16 flex-grow flex flex-col mb-32">
       <slot />
     </main>
 
